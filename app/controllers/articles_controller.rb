317e5f312825ea6a735187600fe29ad635b0d18f
wenzheng-Zhu <zwz_322@126.com>
2020-03-01 14:52:22 +0800

    create new template, action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+
+	def new
+		@article = Article.new
+	end
+
+
+	def create
+		#render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to article_show(@article)
+
+	end
+
+
+	def show
+		@article = Article.find_by(id: params[:id])
+	end
+
+
+
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+
+end
