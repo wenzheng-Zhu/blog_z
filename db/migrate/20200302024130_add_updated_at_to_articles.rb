ee7699a862cada4ab13144905f996b02163636ab
wenzheng-Zhu <zwz_322@126.com>
2020-03-02 11:02:24 +0800

    add user data to articles

---
+++ db/migrate/20200302024130_add_updated_at_to_articles.rb
@@ -0,0 +1,5 @@
+class AddUpdatedAtToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :updated_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_015920) do
+ActiveRecord::Schema.define(version: 2020_03_02_024130) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
