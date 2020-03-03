3fe9a9470f971b9703e7c58c63bbe95a8e2b7859
wenzheng-Zhu <zwz_322@126.com>
2020-03-03 17:15:17 +0800

    add sign in oupon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,8 +34,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sessiion[:id] = @user.id
       flash[:success] = "Welcome to the Blog_z #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
